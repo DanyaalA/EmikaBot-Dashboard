481b0daa0d33e7dbafab88f78e452a217d342128
DanyaalA <Danny13653@gmail.com>
2021-03-13 20:02:23 +0000

    Fixed Sidebar
    Sidebar No longer has the bug where selecting one item selects every item.  The First item is also selected by default

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,25 +1,25 @@
 import * as React from 'react';
 import { SidebarContent, SidebarHeader, SidebarStyle, IconStyle, SidebarItem } from '../utils/styles';
 import { useState } from 'react';
-import { SidebarItem } from "../utils/types";
+import { Route } from "../utils/types";
+import { sidebarItems } from '../utils/constants';
 
-type Props = {
-  items: SidebarItem[];
-}
 
-export const Sidebar = ({ items }: Props) => {
-  const [selected, setSelected] = useState<boolean>(false);
+export const Sidebar = (Pops: any) => {
+  const [selected, setSelected] = useState<Route | undefined>(sidebarItems('552')[0]);
+  const [selectServer, setSelectingServer] = useState<boolean>(true);
+
   return (
     <SidebarStyle>
-      <SidebarHeader>
-        <IconStyle /> <span>Dans Server</span>
+      <SidebarHeader onClick={() => setSelectingServer(!selectServer)}>
+        <IconStyle><img src="https://i.imgur.com/6t1kOaS.png" /></IconStyle> <span>Dans Server</span>
       </SidebarHeader>
       <SidebarContent>
         {/*{props.guilds.map((guild) => (
         <SidebarItem onClick={() => setSelected(!selected)} isSelected={selected}><img src="https://i.imgur.com/XiDt6w6.png"></img><span>{guild.name}</span></SidebarItem>
         ))} */}
         {sidebarItems('552').map((item) => (
-          <SidebarItem onClick={() => setSelected(!selected)} isSelected={selected}><img src="https://i.imgur.com/XiDt6w6.png" /><span>{item.name}</span></SidebarItem>
+          <SidebarItem onClick={() => setSelected(item)} isSelected={selected && item.name == selected.name}><img src="https://i.imgur.com/XiDt6w6.png" /><span>{item.name}</span></SidebarItem>
         ))}
       </SidebarContent>
     </SidebarStyle >
